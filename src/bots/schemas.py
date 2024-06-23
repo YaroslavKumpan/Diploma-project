9afe39ebdb8b31e16500058cdcc0c7ddfd87396c
Uladzislau <uladzislau.savitskiy@outlook.com>
2024-06-23 18:52:18 +0300

    changed body and version type

--- src/bots/schemas.py
+++ src/bots/schemas.py
@@ -6,8 +6,8 @@ from sqlalchemy import Float, String, Text
 class BotBase(BaseModel):
     title: str
     price: float
-    body: Text
-    version: Text
+    body: str
+    version: str
     bot_link: str
 
 
@@ -16,7 +16,41 @@ class BotCreate(BotBase):
 
 
 class Bot(BotBase):
-    model_config = ConfigDict(
-        from_attributes=True,
-    )
+    model_config = ConfigDict(from_attributes=True)
     id: int
+
+
+# class BotBase(BaseModel):
+#     title: Mapped[str] = mapped_column(String(100), unique=False)
+#     price: Mapped[float] = mapped_column(
+#         Float(precision=2),
+#         default=None,
+#         server_default=None,
+#     )
+#     body: Mapped[str] = mapped_column(
+#         Text,
+#         default="",
+#         server_default="",
+#     )
+#     version: Mapped[str] = mapped_column(String(20), unique=False)
+#     update_description: Mapped[str] = mapped_column(
+#         Text,
+#         default="",
+#         server_default="",
+#     )
+
+# class BotUpdate(BotBase):
+#     pass
+#
+#
+# class BotUpdatePartial(BotBase):
+#     name: str | None = None
+#     price: int | None = None
+#     description: str | None = None
+#
+#
+# class BotDelete(BotBase):
+#     model_config = ConfigDict(
+#         from_attributes=True
+#     )  # чтобы возвразался на резульатат с БД, а json
+#     id: int
