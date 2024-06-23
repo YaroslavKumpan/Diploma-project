6205eea7ac334d46d275f0bfaf8b29d24757f811
Uladzislau <uladzislau.savitskiy@outlook.com>
2024-06-23 12:27:06 +0300

    create lifespan

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
+from contextlib import asynccontextmanager
+
 import uvicorn
 from fastapi import FastAPI
 
+from src.core.models import db_helper, Base
 from src.users.views import router as users_router
 
-app = FastAPI()
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    async with db_helper.engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+    yield
+
+
+app = FastAPI(lifespan=lifespan)
 app.include_router(users_router, tags=["Users"])
 
 if __name__ == "__main__":
