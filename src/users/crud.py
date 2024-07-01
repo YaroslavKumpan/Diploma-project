09b3192622b066797f2dbdb5a25a9cce4c896518
Uladzislau <uladzislau.savitskiy@outlook.com>
2024-07-01 13:06:00 +0300

    update create user and add get user and users

--- src/users/crud.py
+++ src/users/crud.py
@@ -1,12 +1,25 @@
-"""
-Create, Read, Update, Delete
-"""
-from src.users.schemas import CreateUser
+from sqlalchemy import select
+from sqlalchemy.engine import Result
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from .models import User
+from .schemas import UserCreate
 
 
-def create_user(user_in: CreateUser) -> dict:
-    user = user_in.model_dump()
-    return {
-        "success": True,
-        "user": user,
-    }
+async def get_users(session: AsyncSession) -> list[User]:
+    stmt = select(User).order_by(User.id)
+    result: Result = await session.execute(stmt)
+    users = result.scalars().all()
+    return list(users)
+
+
+async def get_user(session: AsyncSession, user_id: int) -> User | None:
+    return await session.get(User, user_id)
+
+
+async def create_user(session: AsyncSession, user_in: UserCreate) -> User:
+    user = User(**user_in.model_dump())
+    session.add(user)
+    await session.commit()
+    await session.refresh(user)
+    return user
