37e187b07469f928c05e04accdb33ac47527b230
Uladzislau <uladzislau.savitskiy@outlook.com>
2024-07-03 14:59:58 +0300

    create relationship between user and product

--- src/products/models.py
+++ src/products/models.py
@@ -1,6 +1,12 @@
-from sqlalchemy.orm import Mapped
+from typing import TYPE_CHECKING
+
+from sqlalchemy.orm import Mapped, relationship
 
 from core.models.base import Base
+
+
+if TYPE_CHECKING:
+    from src.users.models import User
 
 
 class Product(Base):
@@ -8,3 +14,5 @@ class Product(Base):
     name: Mapped[str]
     description: Mapped[str]
     price: Mapped[int]
+
+    user: Mapped["User"] = relationship(back_populates="products")
