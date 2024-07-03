fac2f05a84e4cb85929c4af83c781229f52800f2
Uladzislau <uladzislau.savitskiy@outlook.com>
2024-07-03 12:41:40 +0300

    delete import router

--- src/__init__.py
+++
@@ -1,8 +0,0 @@
-from fastapi import APIRouter
-
-from .products.views import router as products_router
-from .users.views import router as users_router
-
-router = APIRouter()
-router.include_router(router=products_router, prefix="/products")
-router.include_router(router=users_router, prefix="/users")
