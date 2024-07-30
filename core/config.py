639ac4934ce23f0010a3d66f321007ce9f251c73
Yaroslav Kumpan <yroslavkumpan@gmail.com>
2024-07-30 11:06:23 +0300

    update config

--- core/config.py
+++ core/config.py
@@ -1,13 +1,26 @@
 from pathlib import Path
+from dotenv import load_dotenv
 from pydantic_settings import BaseSettings
 
+# Определяем базовый каталог
 BASE_DIR = Path(__file__).parent.parent
+
+# Загружаем переменные окружения из .env файла
+load_dotenv()
 
 
 class Settings(BaseSettings):
-    db_url: str = f"sqlite+aiosqlite:///{BASE_DIR}/db.sqlite3"
-    test_db_url: str = f"sqlite+aiosqlite:///{BASE_DIR}/test_db.sqlite3"
-    db_echo: bool = True
+    db_url: str
+    test_db_url: str
+    db_echo: bool
+
+    class Config:
+        env_file = ".env"
 
 
+# Создаём экземпляр настроек
 settings = Settings()
+
+# Формируем полный путь в коде
+settings.db_url = settings.db_url.replace("///", f"///{BASE_DIR}/")
+settings.test_db_url = settings.test_db_url.replace("///", f"///{BASE_DIR}/")
